0f6a8ef91e103f06e8be338b646b20b15c24cf93
Sean Jungbluth <jungbluth.sean@gmail.com>
2021-03-30 09:41:31 -0700

    Added or cleaned shebangs

--- CRUX_Formatted_database_comparisions/CRUX_CO1_test_blca_sub.sh
+++ CRUX_Formatted_database_comparisions/CRUX_CO1_test_blca_sub.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 source /u/local/Modules/default/init/bash
 module load ATS
@@ -15,7 +15,7 @@ do
  j=${str2#/u/project/rwayne/eecurd/eecurd/five-fold-test/CO1_5-fold/}
  echo ${j}
  echo ${h} "..."
- echo 
- printf " #!/bin/bash\n#$ -l highp,h_rt=15:00:00,h_data=48G\n#$ -N r${j}_sub${h}_anacapa_class_test\n#$ -cwd\n#$ -m bea\n#$ -M eecurd\n#$ \n#$ -o /u/project/rwayne/eecurd/eecurd/five-fold-test_anacapa_blca/Run/runlog/${j}_sub${h}run.out.txt\n#$ -e /u/project/rwayne/eecurd/eecurd/five-fold-test_anacapa_blca/Run/runlog/${j}_sub${h}run.err.txt\n\nsh /u/project/rwayne/eecurd/eecurd/five-fold-test_anacapa_blca/scripts/run_5-fold_db_tests_anacapa.sh -m /u/project/rwayne/eecurd/eecurd/five-fold-test_anacapa_blca/ -n ${j}_sub${h}  -s /u/project/rwayne/eecurd/eecurd/five-fold-test/CO1_5-fold/CO1_5-fold_1/subset_${h} " > /u/project/rwayne/eecurd/eecurd/five-fold-test_anacapa_blca/Run/runscript/${j}_sub${h}_blca.sh 
- qsub /u/project/rwayne/eecurd/eecurd/five-fold-test_anacapa_blca/Run/runscript/${j}_sub${h}_blca.sh 
+ echo
+ printf " #!/bin/bash\n#$ -l highp,h_rt=15:00:00,h_data=48G\n#$ -N r${j}_sub${h}_anacapa_class_test\n#$ -cwd\n#$ -m bea\n#$ -M eecurd\n#$ \n#$ -o /u/project/rwayne/eecurd/eecurd/five-fold-test_anacapa_blca/Run/runlog/${j}_sub${h}run.out.txt\n#$ -e /u/project/rwayne/eecurd/eecurd/five-fold-test_anacapa_blca/Run/runlog/${j}_sub${h}run.err.txt\n\nsh /u/project/rwayne/eecurd/eecurd/five-fold-test_anacapa_blca/scripts/run_5-fold_db_tests_anacapa.sh -m /u/project/rwayne/eecurd/eecurd/five-fold-test_anacapa_blca/ -n ${j}_sub${h}  -s /u/project/rwayne/eecurd/eecurd/five-fold-test/CO1_5-fold/CO1_5-fold_1/subset_${h} " > /u/project/rwayne/eecurd/eecurd/five-fold-test_anacapa_blca/Run/runscript/${j}_sub${h}_blca.sh
+ qsub /u/project/rwayne/eecurd/eecurd/five-fold-test_anacapa_blca/Run/runscript/${j}_sub${h}_blca.sh
 done
